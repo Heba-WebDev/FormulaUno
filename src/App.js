674bff9674726804acc4642099c2565c5545395a
Heba-Webdev23 <jtvonline@yahoo.com>
2022-06-17 03:45:27 +0300

    finished next race route page

--- src/App.js
+++ src/App.js
@@ -21,7 +21,15 @@ function App() {
  
   let today = new Date();
   let todaysDate = `${today.getFullYear()}-${today.getMonth()+1 < 10 ? `0${today.getMonth()+1}` : `${today.getMonth()+1}`}-${today.getDate() < 10 ? `0${today.getDate()}` : `${today.getDate()}`}`;
-
+   
+  let FirstPractice = '';
+  let FirstPracticeTime = '';
+  let SecondPractice = '';
+  let SecondPracticeTime = '';
+  let ThirdPractice = '';
+  let ThirdPracticeTime = '';
+  let Qualifying = '';
+  let QualifyingTime = '';
   
   React.useEffect(() => {
    fetch('http://ergast.com/api/f1/current.json')
@@ -64,17 +72,37 @@ function App() {
   for(let i=0; i < races.length; i++) {
     if(races[i].date > todaysDate) {
       nextRace = races[i];
-      //console.log(nextRace)
+      FirstPractice = races[i].FirstPractice.date;
+      FirstPracticeTime = races[i].FirstPractice.time;
+      SecondPractice = races[i].SecondPractice.date;
+      SecondPracticeTime = races[i].SecondPractice.time;
+      ThirdPractice = races[i].ThirdPractice.date;
+      ThirdPracticeTime = races[i].ThirdPractice.time;
+      Qualifying = races[i].Qualifying.date;
+      QualifyingTime = races[i].Qualifying.time;
       break;
     }
   }
-
+ 
+  
   return (
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<Navbar />}>
     <Route index element={<Main />} />
-    <Route path="next-race" element={<NextRace date={nextRace.date}/>} />
+    <Route path="next-race" element={<NextRace
+     first={FirstPractice}
+     firsttime={FirstPracticeTime}
+     second={SecondPractice}
+     secondtime={SecondPracticeTime}
+     third={ThirdPractice}
+     thirdtime={ThirdPracticeTime}
+     qualify={Qualifying}
+     qualifytime={QualifyingTime}
+     date={nextRace.date}
+     time={nextRace.time}
+    
+     />} />
     <Route path="driver-standings" element={<DriverStandings />} />
     <Route path="constructor-standings" element={<ConstructorStandings />} />
     <Route path="calendar" element={<FullCalender />} />
--- src/Components/NextRace.js
+++ src/Components/NextRace.js
@@ -4,37 +4,42 @@ import React from 'react'
 
 export default function NextRace(props) {
   
-  //   let nextRace = [];
-  //   const [races, setRaces] = React.useState([]);
-  //   React.useEffect(() => {
-  //       fetch('http://ergast.com/api/f1/current.json')
-  //        .then(response => response.json())
-  //        .then(data => {
-  //          setRaces(data.MRData.RaceTable.Races);
-  //        })
-  //         .catch(error => console.log(error))
-  //      },[])
-
-  //      let today = new Date();
-  // let todaysDate = `${today.getFullYear()}-${today.getMonth()+1 < 10 ? `0${today.getMonth()+1}` : `${today.getMonth()+1}`}-${today.getDate() < 10 ? `0${today.getDate()}` : `${today.getDate()}`}`;
-
-  // for(let i=0; i < races.length; i++) {
-  //   if(races[i].date > todaysDate) {
-  //     nextRace = races[i];
-  //     console.log(nextRace)
-  //     break;
-  //   }
-  // }
-  
-  console.log(props.date)
-
        
     return (
 
-        <div>
+        <div className='grid grid-col-2 gap-2 text-red-900'>
      
      
-   <h3>hi</h3>
+        <div className='flex flex-col bg-yellow-400 rounded p-2'>
+       <small className='text-white'>1st Practice</small>
+       <h4 className='m-auto text-l p-1'>{props.first}</h4>
+       <h4 className='m-auto text-l p-1'>{props.firsttime}</h4>
+       </div>
+
+       <div className='flex flex-col bg-yellow-400 rounded p-2'>
+       <small className='text-white'>2nd Practice</small>
+       <h4 className='m-auto text-l p-1'>{props.second}</h4>
+       <h4 className='m-auto text-l p-1'>{props.secondtime}</h4>
+       </div>
+
+       <div className='flex flex-col bg-yellow-400 rounded p-2'>
+       <small className='text-white'>3rd Practice</small>
+       <h4 className='m-auto text-l p-1'>{props.third}</h4>
+       <h4 className='m-auto text-l p-1'>{props.thirdtime}</h4>
+       </div>
+      
+
+       <div className='flex flex-col bg-lime-300 rounded p-2'>
+       <small className='text-white'>Qualifying</small>
+       <h4 className='m-auto text-l p-1'>{props.qualify}</h4>
+       <h4 className='m-auto text-l p-1'>{props.qualifytime}</h4>
+       </div> 
+
+       <div className='col-span-2 flex flex-col bg-green-500 rounded p-2'>
+       <small className='text-white'>Race</small>
+       <h4 className='m-auto text-l p-1'>{props.date}</h4>
+       <h4 className='m-auto text-l p-1'>{props.time}</h4>
+       </div>
 
         </div>
     )
