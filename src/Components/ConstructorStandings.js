2dd1833aa0485f54dabeace3117df1fb57af01fd
Heba-Webdev23 <jtvonline@yahoo.com>
2022-10-14 05:21:26 +0300

    added react query to both driver and constructor standings components

--- src/Components/ConstructorStandings.js
+++ src/Components/ConstructorStandings.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 export default function ConstructorStandings(props) {
 
-    const [constructorsStanding, setConstructorsStanding] = React.useState([]);
-    React.useEffect(() => {
-        fetch('http://ergast.com/api/f1/current/constructorStandings.json')
-        .then(response => response.json())
-        .then(data => {
-      // console.log(data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings)
-         setConstructorsStanding(data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings)
-        })
-      },[])
+    // const [constructorsStanding, setConstructorsStanding] = React.useState([]);
+    // React.useEffect(() => {
+    //     fetch('http://ergast.com/api/f1/current/constructorStandings.json')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //   // console.log(data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings)
+    //      setConstructorsStanding(data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings)
+    //     })
+    //   },[])
+
+      const fetchConstructorStandings = async () => {
+        const response = await fetch('http://ergast.com/api/f1/current/constructorStandings.json');
+        return response.json()
+      }
+    
+      const constructorStandings = useQuery(['constructorStandings'], fetchConstructorStandings);
+    
+
+      if(constructorStandings.isLoading) {
+        return <div>Loading ...</div>
+    }
+
+    if(constructorStandings.isError) {
+        return <div>Error ...</div>
+    }
+     
 
 return (
    
 <div className='container m-auto'>
 
-{constructorsStanding.map((constructor) => {
+{constructorStandings.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings.map((constructor) => {
     return (
         <div key={constructor.Constructor.constructorId} className='bg-slate-50 text-left flex 
         mb-1'>
--- src/Components/DriverStandings.js
+++ src/Components/DriverStandings.js
@@ -1,21 +1,38 @@
 import React from 'react';
-
+import { useQuery } from '@tanstack/react-query';
 
 export default function DriverStandings() {
-    const [drivesStanding, setDriversStanding] = React.useState([]);
-    React.useEffect(() => {
-        fetch('http://ergast.com/api/f1/current/driverStandings.json')
-        .then(response => response.json())
-        .then(data => {
-         // console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings)
-          setDriversStanding(data.MRData.StandingsTable.StandingsLists[0].DriverStandings)
-        })
-        .catch(error => console.log(error))
-      },[])
+    // const [drivesStanding, setDriversStanding] = React.useState([]);
+    // React.useEffect(() => {
+    //     fetch('http://ergast.com/api/f1/current/driverStandings.json')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //      // console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings)
+    //       setDriversStanding(data.MRData.StandingsTable.StandingsLists[0].DriverStandings)
+    //     })
+    //     .catch(error => console.log(error))
+    //   },[])
+
+      const fetchDriverStandings = async () => {
+        const response = await fetch('http://ergast.com/api/f1/current/driverStandings.json');
+        return response.json()
+      }
+    
+      const driverStandings = useQuery(['dirverStandings'], fetchDriverStandings);
+    
+
+      if(driverStandings.isLoading) {
+        return <div>Loading ...</div>
+    }
+
+    if(driverStandings.isError) {
+        return <div>Error ...</div>
+    }
+     
 return (
    <div className='container  m-auto'>
 
-      {drivesStanding.map((driver) => {
+      {driverStandings.data.MRData.StandingsTable.StandingsLists[0].DriverStandings.map((driver) => {
           return (
             <div key={driver.Driver.driverId} className='bg-slate-50 text-left flex
             content-start mb-1'>
@@ -32,7 +49,7 @@ return (
                 
             </div>
           )
-      })}
+      })} 
    </div>
 )
 
